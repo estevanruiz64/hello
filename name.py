22dd680771e0ce159e3f9dcd0ad3e88a571cd38a
Estevan Ruiz <econ.eruiz@gmail.com>
2022-06-02 17:57:09 -0500

    testing atom commits

---
+++ name.py
@@ -0,0 +1,8 @@
+n = int(input("Number: "))
+
+if n > 0:
+    print("THE NUMBER IS POSITIVE BABY!")
+elif n < 0:
+    print("N IS NEGATIVE OH NO")
+else:
+    print("it's zero... it's always been zero")
