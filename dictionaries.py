487ee93efc546802189bf97b66c2c4bbec761a28
Estevan Ruiz <econ.eruiz@gmail.com>
2022-06-03 14:01:49 -0500

    class notes

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+houses["Hermione"] = "Gryffindor"
+
+s = input("Name: ")
+
+print(houses[s])
---
+++ loops.py
@@ -0,0 +1,12 @@
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+for name in names:
+    print(name)
+
+name = "Harry"
+
+for n in name:
+    print(n)
